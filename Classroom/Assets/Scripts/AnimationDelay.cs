6d6713b61065309d0d554697c20d02a16fdc1570
isabell nguyen <isabell.nguyen@stud.h-da.de>
2020-07-11 23:41:02 +0200

    implemented bubble dialogue

---
+++ Classroom/Assets/Scripts/AnimationDelay.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationDelay : MonoBehaviour
+{
+    //public int startDelay;
+    //Animator animator;
+    Animator m_Animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //yield WaitForSeconds(4);
+        //animation.Play("bubble");
+        //animator = GetComponent<Animator>();
+        //StartCoroutine(DelayedAnimation());
+        m_Animator = gameObject.GetComponent<Animator>();
+    }
+
+    /*IEnumerator DelayedAnimation ()
+     {
+         yield return new WaitForSeconds(startDelay);
+         animator.Play("animation_I_want_to_play");
+     }*/
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+}
---
+++ Classroom/Assets/Scripts/bubble.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bubble : MonoBehaviour
+{
+
+    private Animator anim;
+    public StoryDialogeManager dialouge;
+    public bool dialougeStarted=false;
+    public bool dialougeContinue=true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator> ();
+        dialouge = dialouge.GetComponent<StoryDialogeManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (anim.GetCurrentAnimatorStateInfo(0).IsName("bubble"))
+        {
+            Debug.Log("running");
+        }
+        else
+        {
+            if(dialougeStarted==false)
+            {
+                Debug.Log("ende");
+                
+                dialouge.npcIndex = 0;
+
+                dialouge.StartCoroutine(dialouge.StartDialouge());
+                dialougeStarted = true;
+            }
+            else
+            {
+                if (dialouge.continueDialouge==true)
+                {
+                    dialouge.TriggerContinueDialogue();
+                    dialougeContinue = false;
+
+                }
+            }
+        }
+    }
+}
