725d7d592220002e6786eeadad0dacbd058c9b64
isabell nguyen <isabell.nguyen@stud.h-da.de>
2020-07-13 14:03:07 +0200

    pause menu
    + added pause menu with resume and quit option

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//based on a tutorial by Brackeys on Youtube https://www.youtube.com/watch?v=JivuXdrIHK0
+
+public class PauseMenu : MonoBehaviour
+{
+   public static bool GameIsPaused = false;
+   public GameObject PauseMenuUI;
+   public GameObject teacher;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(GameIsPaused)
+            {
+                Resume();
+            } else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+        teacher.GetComponent<AudioSource>().UnPause(); 
+    }
+    void Pause()
+    {
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+        teacher.GetComponent<AudioSource>().Pause(); 
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting Game..");
+        Application.Quit();
+    }
+}
--- Assets/classroom/Assets/Scripts/teacher.cs
+++ Assets/classroom/Assets/Scripts/teacher.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Lehrer : MonoBehaviour
+public class teacher : MonoBehaviour
 {
     public AudioSource audio;
 
